3538b3315221a3f5d1342714e2d7149f30eadbf4
xieguigang <xie.guigang@gcmodeller.org>
2021-07-01 20:45:07 +0800

    get source tokens

--- codegraph/VBGraph.vb
+++ codegraph/VBGraph.vb
@@ -1,3 +1,38 @@
-﻿Public Class VBGraph
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
+Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.vbproj
+Imports Microsoft.VisualBasic.Data.NLP
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text
 
-End Class
+Public Module VBGraph
+
+    Public Function LoadProject(vbproj As String) As String()
+        Dim proj As Project = vbproj.LoadXml(Of Project)
+        Dim vb As String() = proj _
+            .EnumerateSourceFiles(skipAssmInfo:=True) _
+            .ToArray
+        Dim dir As String = vbproj.ParentPath
+
+        vb = vb _
+            .Select(Function(filename)
+                        Return $"{dir}/{filename}".GetFullPath
+                    End Function) _
+            .ToArray
+
+        Return vb
+    End Function
+
+    Public Function SourceTokens(file As String) As String()
+        Return file _
+            .SolveStream _
+            .Split(" "c, ASCII.CR, ASCII.LF, ASCII.TAB, """"c, "'"c,
+                   "+"c, "-"c, "*"c, "/"c, "("c, ")"c, "["c, "]"c, "?"c,
+                   "."c, "<"c, ">"c, ","c, "!"c, ":"c, "="c, "{"c, "}"c,
+                   "#"c, "\"c, "&"c, "^"c, "%"c, "$"c, "@"c
+             ) _
+            .Where(Function(s) Not s.StringEmpty) _
+            .DoCall(AddressOf StopWords.DefaultStopWords.DefaultValue.Removes) _
+            .ToArray
+    End Function
+
+End Module
---
+++ test/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports codegraph
+
+Module Module1
+
+    Sub Main()
+        Dim vb = VBGraph.LoadProject("E:\GCModeller\src\runtime\sciBASIC#\Microsoft.VisualBasic.Core\src\47-dotnet_Microsoft.VisualBasic.vbproj")
+        Dim tokens As String() = VBGraph.SourceTokens(vb(Scan0))
+
+        Pause()
+    End Sub
+
+End Module
