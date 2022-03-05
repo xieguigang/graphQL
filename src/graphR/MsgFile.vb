7e1c76f59886b6bd295e09cf870cb82514336697
xieguigang <xie.guigang@gcmodeller.org>
2022-03-05 13:30:12 +0800

    Update MsgFile.vb

--- src/graphR/MsgFile.vb
+++ src/graphR/MsgFile.vb
@@ -1,10 +1,31 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports System.IO
+Imports graphMsg
+Imports graphQL
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("MsgFile")>
 Module MsgFile
 
-    Public Function open()
+    <ExportAPI("open")>
+    Public Function open(<RRawVectorArgument>
+                         Optional file As Object = Nothing,
+                         Optional env As Environment = Nothing) As Object
 
+        If file Is Nothing Then
+            Return New GraphPool({}, {})
+        Else
+            Dim buffer = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
+
+            If buffer Like GetType(Message) Then
+                Return buffer.TryCast(Of Message)
+            Else
+                Return StorageProvider.Open(buffer)
+            End If
+        End If
     End Function
 
 End Module
