723a82483ce0dc634b66efc991d0cd9877586faa
xieguigang <xie.guigang@gcmodeller.org>
2022-03-05 13:54:28 +0800

    Update createDb.py

--- test/createDb.py
+++ test/createDb.py
@@ -1,2 +1,11 @@
 import graphQL
 
+setwd(@dir)
+
+kegg = read.csv("./kegg.csv", row.names = None)
+hmdb = read.csv("./hmdb.csv", row.names = None)
+chebi = read.csv("./chebi.csv", row.names = None)
+
+print(kegg)
+print(hmdb)
+print(chebi)
