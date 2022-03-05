cf64362ad060c7889c50041fa88c2fc63c7aca7e
xieguigang <xie.guigang@gcmodeller.org>
2022-03-05 13:33:42 +0800

    how to create knowledge insert into?

--- src/graphR/Query.vb
+++ src/graphR/Query.vb
@@ -1,8 +1,23 @@
+Imports graphQL
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 <Package("Query")>
 Public Module Query
 
+    ''' <summary>
+    ''' insert a knowledge node into the graph pool
+    ''' </summary>
+    ''' <param name="knowledge"></param>
+    ''' <param name="meta"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("insert")>
+    Public Function insert(kb As GraphPool, knowledge As String,
+                           Optional meta As list = Nothing,
+                           Optional env As Environment = Nothing) As Object
 
+    End Function
 
 End Module
