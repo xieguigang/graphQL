e394e839ac8639b19411b5972ddd106fa9c0f8f0
xieguigang <xie.guigang@gcmodeller.org>
2022-03-05 14:24:41 +0800

    create a knowledge network

--- src/graphQL/GraphPool.vb
+++ src/graphQL/GraphPool.vb
@@ -1,6 +1,6 @@
 Imports Microsoft.VisualBasic.Data.GraphTheory
 
-Public Class GraphPool : Inherits Graph(Of Knowledge, Association)
+Public Class GraphPool : Inherits Graph(Of Knowledge, Association, GraphPool)
 
     Sub New(knowledge As IEnumerable(Of Knowledge), links As IEnumerable(Of VertexEdge))
         For Each kb As Knowledge In knowledge
@@ -21,7 +21,36 @@ Public Class GraphPool : Inherits Graph(Of Knowledge, Association)
     ''' term.
     ''' </param>
     Public Sub AddKnowledge(knowledge As String, meta As Dictionary(Of String, String()))
-        Dim term As Knowledge
+        Dim term As Knowledge = ComputeIfAbsent(knowledge)
+
+        For Each info As KeyValuePair(Of String, String()) In meta
+            For Each data As String In info.Value
+                Dim metadata As Knowledge = ComputeIfAbsent(data)
+                Dim refId As String = VertexEdge.EdgeKey(metadata, term)
+                Dim link As Association
+
+                If edges.ContainsKey(refId) Then
+                    link = edges(refId)
+                    link.weight += 1
+                Else
+                    link = New Association With {
+                        .type = info.Key,
+                        .U = metadata,
+                        .V = term,
+                        .weight = 1
+                    }
+                    Call Me.Insert(link)
+                End If
+            Next
+        Next
     End Sub
+
+    Private Function ComputeIfAbsent(term As String) As Knowledge
+        If Me.ExistVertex(term) Then
+            Return Me.vertices(term)
+        Else
+            Return Me.AddVertex(term)
+        End If
+    End Function
 
 End Class
