572f1921db3160b51869c95f3d42a6e2c108537a
xieguigang <xie.guigang@gcmodeller.org>
2022-03-14 10:25:50 +0800

    Update MsgFile.vb

--- src/graphR/MsgFile.vb
+++ src/graphR/MsgFile.vb
@@ -23,8 +23,6 @@ Module MsgFile
                          Optional file As Object = Nothing,
                          Optional env As Environment = Nothing) As Object
 
-        Throw New NotImplementedException("test for exception")
-
         If file Is Nothing Then
             Return New GraphPool({}, {})
         Else
