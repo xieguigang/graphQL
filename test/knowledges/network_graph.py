d3f1c3b0ffbae482712e803e8dc5b62954d57133
xieguigang <xie.guigang@gcmodeller.org>
2022-03-16 13:17:26 +0800

    run network test

--- test/knowledges/network_graph.py
+++ test/knowledges/network_graph.py
@@ -5,6 +5,9 @@ setwd(@dir)
 
 kb = MsgFile::open(`${@dir}/aspirin.graph`)
 g = networkGraph(kb)
-g = Kosaraju.SCCs(g)
+result = knowledgeCommunity(g, common.type = ["formula","chembl","chebi","knapsack"], eps = 1e-8)
 
+g = result$graph
+
+write.csv(result$knowledges, file = "./knowledges.xls")
 save.network(g, file = "./aspirin_graph/", properties = "color")
