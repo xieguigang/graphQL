1a602f500608c8e0a4f8bafe4801cdd041a08599
xieguigang <xie.guigang@gcmodeller.org>
2022-03-23 09:51:55 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,10 @@
-imports ["Query", "MsgFile"] from "graphR";
+imports ["Query", "MsgFile", "KnowledgeGraph"] from "graphR";
 
 const .onLoad = function() {
-
+	cat("\n");
+	cat("NoSQL graph database engine writen in VisualBasic\n");
+	cat("for R# language\n");
+	cat("\n");
+	cat("github: https://github.com/xieguigang/graphQL\n");
+	cat("\n");
 }
