77054adaee987ffbbc047981438e8d00e606468b
xieguigang <xie.guigang@gcmodeller.org>
2022-04-14 23:08:55 +0800

    improvements o the demo test

--- test/getEdgeSource.R
+++ test/getEdgeSource.R
@@ -1,10 +1,14 @@
 require(graphQL);
 
 printLinksSource = function(path) {
-print(path);
-print(edgeSource(path));
+	print(path);
+	# print(substr(edgeSource(path), 1, 5));
+	print(edgeSource(path));
+}
+
+for(file in list.files("D:\Database\20220306\multiples\graphDb", pattern = "*.graph")) {
+printLinksSource(file);
 }
 
 
-printLinksSource("D:\biodeep\graphdb\multiples\graphDb\knapsack_base.graph");
-printLinksSource("D:\biodeep\graphdb\multiples\graphDb\flavonoid_base.graph");
+# printLinksSource("D:\biodeep\graphdb\multiples\graphDb\flavonoid_base.graph");
