c80accf346591a52ab5c69f8ca3d1d57a0ea7b97
guigang xie <gg.xie@bionovogene.com>
2022-06-28 11:15:07 +0800

    read text data test success

--- test/knowledges/viewdb.py
+++ test/knowledges/viewdb.py
@@ -1,5 +1,6 @@
 from base import HDS
 import console
+import JSON
 
 setwd(@dir)
 
@@ -9,3 +10,6 @@ cat("\n\n")
 
 # print(HDS::files(hds))
 console::log(HDS::tree(hds))
+
+str(json_decode(HDS::getText(hds, "/knowledge_blocks.json")))
+str(json_decode(HDS::getText(hds, "/summary.json")))
