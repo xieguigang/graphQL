025e4d8ffc8f03ac17bbc2ba54990a460845862b
guigang xie <gg.xie@bionovogene.com>
2022-06-28 11:31:16 +0800

    Create TermIndexMsg.vb

---
+++ src/graphMsg/Message/TermIndexMsg.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.Data.IO.MessagePack.Serialization
+
+Namespace Message
+
+    Public Class TermIndexMsg
+
+        <MessagePackMember(0)> Public Property index As TermIndex()
+
+    End Class
+
+    Public Class TermIndex
+
+        <MessagePackMember(0)> Public Property term As String
+        <MessagePackMember(1)> Public Property block As String
+        <MessagePackMember(2)> Public Property offset As Integer
+        <MessagePackMember(3)> Public Property size As Integer
+
+    End Class
+End Namespace
