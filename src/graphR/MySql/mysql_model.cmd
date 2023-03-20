ad14e308ed7e3f47ef81ca549edc48e6ff51cc82
xieguigang <gg.xie@bionovogene.com>
2023-03-20 17:34:39 +0800

    export mysql models

---
+++ src/graphR/MySql/mysql_model.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+SET Reflector="../../mysqli/App/Reflector.exe"
+SET sql_src="../../web/mysql/graphdb.sql"
+
+%Reflector% --reflects /sql %sql_src% -o ./ /namespace graphdb --language visualbasic /split
