3052933bd44abbd499ad8882b20f689f5a361f42
xieguigang <gg.xie@bionovogene.com>
2023-03-22 20:46:10 +0800

    updates of the database schema

--- src/graphR/MySql/mysql_model.cmd
+++ src/graphR/MySql/mysql_model.cmd
@@ -4,3 +4,5 @@ SET Reflector="../../mysqli/App/Reflector.exe"
 SET sql_src="../../web/mysql/graphdb.sql"
 
 %Reflector% --reflects /sql %sql_src% -o ./ /namespace graphdb --language visualbasic /split
+
+pause
