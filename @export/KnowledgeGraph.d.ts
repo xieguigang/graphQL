350b3edaf7e24d90e1370561b6c9c292cf6c68d6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-10 10:56:02 +0800

    build document and package export headers

--- @export/KnowledgeGraph.d.ts
+++ @export/KnowledgeGraph.d.ts
@@ -1,5 +1,7 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
+//    imports "KnowledgeGraph" from "graphR";
+//
 // ref=graphR.KnowledgeGraph@graphR, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
