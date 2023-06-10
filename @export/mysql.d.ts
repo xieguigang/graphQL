82a8f5dec22014523e0e71a0f5c97bfda52c4539
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-10 11:02:31 +0800

    build package export headers

---
+++ @export/mysql.d.ts
@@ -0,0 +1,22 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "mysql" from "graphR";
+//
+// ref=graphR.mysqlDatabase@graphR, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace mysql {
+   /**
+    * dump the inserts transaction mysql file
+    * 
+    * 
+     * @param data A collection of the mysql row data for insert into the database
+     * @param dir -
+     * @param env -
+     * 
+     * + default value Is ``null``.
+   */
+   function dump_inserts(data: any, dir: string, env?: object): any;
+}
---
+++ man/graphR/mysql/mysql.md
@@ -0,0 +1,5 @@
+# mysql
+
+
+
++ [dump_inserts](mysql/dump_inserts.1) dump the inserts transaction mysql file
