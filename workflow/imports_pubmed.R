d04cc666e18c0b78c7e4c2bc34d3e41bd0aa5d5a
guigang xie <xie.guigang@gcmodeller.org>
2023-09-12 17:21:43 +0800

    Update imports_pubmed.R

--- workflow/imports_pubmed.R
+++ workflow/imports_pubmed.R
@@ -5,5 +5,15 @@ imports "pubmed" from "kb";
 
 let articles = as.data.frame(pubmed::parse(readText(`${@dir}/pubmed-Aspirin-set.txt`)));
 
-print(articles);
+print(articles$abstract);
 
+for(str in articles$abstract) {
+    let g = text_graph(str);
+
+    for(par in g) {
+        print(par);
+        graphQL::push_graph(par);
+    }
+
+    # stop();
+}
