eb60a8b83cb8be240b7c2711cd2493bd81af1310
guigang xie <xie.guigang@gcmodeller.org>
2023-09-12 13:28:50 +0800

    create the text graph data

--- R/graphKbWeb.R
+++ R/graphKbWeb.R
@@ -1,6 +1,76 @@
 # web api handler for create graph kb
 const text_graph = function(text) { 
-    let tokens = strsplit(text, "\s+");
+    let tokens = text 
+    |> trim(characters = " ,.?;'") 
+    |> strsplit("\s+")
+    ;
     
-    print(tokens);
+    if (is.list(tokens)) {
+        tokens = tokens |> lapply(t -> tokens_trim(t));
+    } else {
+        tokens = list(single = tokens_trim(tokens)); 
+    }    
+
+    lapply(tokens, t -> one_graph(t));
+}
+
+const tokens_trim = function(tokens) {
+    tokens = sapply(tokens, si -> si |> trim(characters = " ,.?;'()"));
+    tokens = tokens[nchar(tokens) > 0];
+
+    tolower(tokens);
+}
+
+#' The text data will be indexed via the FNV-1a hash algorithm
+#' 
+#' @return a dataframe object that contains the graph link data
+#' 
+const one_graph = function(tokens, phase_size = 3) {
+    const g = list();    
+
+    for(i in phase_size:[length(tokens) - 1]) {
+        g[[length(g) + 1]] = graph_link(tokens[i], tokens[i+1], w = 1);
+
+        # from
+        for (j in [i-phase_size]:i) {
+            g[[length(g) + 1]] = graph_link(tokens[j], tokens[j+1], w = 1 / [i - j]);
+        }
+        # to
+        for(j in i:[i + phase_size]) {
+            g[[length(g) + 1]] = graph_link(tokens[j], tokens[j+1], w = 1 / [j - i]);
+        }
+    }
+
+    const graph_df = data.frame(
+        from = g@from,
+        to = g@to,
+        from_i = g@from_i,
+        to_i = g@to_i,
+        w = g@w,
+        index = `${g@from_i} | ${g@to_i}`
+    );
+    const meltdown = graph_df |> groupBy("index");
+    const graph_out = data.frame(
+        from = sapply(meltdown, d -> .Internal::first(d$from)),
+        to = sapply(meltdown, d -> .Internal::first(d$to)),
+        from_i = sapply(meltdown, d -> .Internal::first(d$from_i)),
+        to_i = sapply(meltdown, d -> .Internal::first(d$to_i)),
+        w = sapply(meltdown, d -> sum(d$w)),
+        size = sapply(meltdown, d -> nrow(d))
+    );
+
+    graph_out;
+}
+
+const graph_link = function(from, to, w = 1) {
+    if (max(as.integer(is.null(from)), as.integer(is.null(to))) == 1) {
+        NULL;
+    } else {
+        list(
+            from = from, to = to,
+            from_i = FNV1a_hashcode(from),
+            to_i = FNV1a_hashcode(to),
+            w = w
+        );
+    }
 }
---
+++ workflow/save_text.R
@@ -0,0 +1,21 @@
+require(graphQL);
+require(JSON);
+
+let text = [
+    "Water appears as a clear, nontoxic liquid composed of hydrogen and oxygen, essential for life and the most widely used solvent. Include water in a mixture to learn how it could react with other chemicals in the mixture.",
+"Water is an oxygen hydride consisting of an oxygen atom that is covalently bonded to two hydrogen atoms It has a role as an amphiprotic solvent, a member of greenhouse gas, a human metabolite, a Saccharomyces cerevisiae metabolite, an Escherichia coli metabolite and a mouse metabolite. It is an oxygen hydride, a mononuclear parent hydride and an inorganic hydroxy compound. It is a conjugate base of an oxonium. It is a conjugate acid of a hydroxide.",
+"	
+Water appears as a clear, nontoxic liquid composed of hydrogen and oxygen, essential for life and the most widely used solvent. Include water in a mixture to learn how it could react with other chemicals in the mixture.
+CAMEO Chemicals
+Water is an oxygen hydride consisting of an oxygen atom that is covalently bonded to two hydrogen atoms It has a role as an amphiprotic solvent, a member of greenhouse gas, a human metabolite, a Saccharomyces cerevisiae metabolite, an Escherichia coli metabolite and a mouse metabolite. It is an oxygen hydride, a mononuclear parent hydride and an inorganic hydroxy compound. It is a conjugate base of an oxonium. It is a conjugate acid of a hydroxide.
+ChEBI
+Water (chemical formula: H2O) is a transparent fluid which forms the world's streams, lakes, oceans and rain, and is the major constituent of the fluids of organisms. As a chemical compound, a water molecule contains one oxygen and two hydrogen atoms that are connected by covalent bonds. Water is a liquid at standard ambient temperature and pressure, but it often co-exists on Earth with its solid state, ice; and gaseous state, steam (water vapor).",
+"Water is a metabolite found in or produced by Escherichia coli (strain K12, MG1655).",
+"Water is h2O, a clear, colorless, odorless, tasteless liquid that freezes into ice below 0 degrees centigrade and boils above 100 degrees centigrade.",
+"A clear, odorless, tasteless liquid that is essential for most animal and plant life and is an excellent solvent for many substances. The chemical formula is hydrogen oxide (H2O). (McGraw-Hill Dictionary of Scientific and Technical Terms, 4th ed)"
+];
+let g = text_graph(text);
+
+for(par in g) {
+    print(par);
+}
