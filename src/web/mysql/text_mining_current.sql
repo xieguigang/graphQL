56650bf68739706b41d035dc896cc98d7b114577
guigang xie <xie.guigang@gcmodeller.org>
2023-09-19 08:27:13 +0800

    config database schema update comparision

---
+++ src/web/mysql/text_mining_current.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `text_mining` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `text_mining`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 192.168.0.231    Database: text_mining
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `text_graph`
+--
+
+DROP TABLE IF EXISTS `text_graph`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `text_graph` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `from` int unsigned NOT NULL,
+  `to` int unsigned NOT NULL,
+  `weight` double unsigned NOT NULL DEFAULT '0',
+  `add_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `query_link` (`from`,`to`)
+) ENGINE=InnoDB AUTO_INCREMENT=9167 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `word_token`
+--
+
+DROP TABLE IF EXISTS `word_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `word_token` (
+  `id` int unsigned NOT NULL,
+  `token` varchar(1024) NOT NULL,
+  `count` int NOT NULL,
+  `add_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `token_UNIQUE` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='A word token in a given text segment';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-19  8:26:03
---
+++ src/web/mysql/text_mining_current_upgrades_to_text_mining.schema_compares.report.md
@@ -0,0 +1,25 @@
+﻿# Schema update report
+
+Update report for schema of ``text_mining_current.sql`` updates to new model ``text_mining.sql``
+### Updates for ``text_graph``
+
+Field data attribute of current table ``weight`` has been updated:
+
+```sql
+ALTER TABLE `text_mining`.`text_graph` CHANGE COLUMN `weight` `weight` double UNSIGNED NOT NULL DEFAULT 0 COMMENT '' ;
+```
+
+Add a new data field ``count``:
+
+```sql
+ALTER TABLE `text_mining`.`text_graph` ADD COLUMN `count` int (11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '' ;
+```
+
+### Updates for ``word_token``
+
+Update table description comment:
+
+```sql
+ALTER TABLE `text_mining`.`word_token` COMMENT = 'A word token in a given text segment' ;
+```
+
