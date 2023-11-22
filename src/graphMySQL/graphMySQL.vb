c0d3910de21de5f6ed299618418240f09778fb54
谢桂纲 <gg.xie@bionovogene.com>
2023-11-22 15:23:52 +0800

    how to add graph data ?

---
+++ src/graphMySQL/graphMySQL.vb
@@ -0,0 +1,22 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.Uri
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+
+Public Class graphMySQL
+
+    Public ReadOnly Property graph As Model
+    Public ReadOnly Property hash_index As Model
+    Public ReadOnly Property knowledge As Model
+    Public ReadOnly Property knowledge_vocabulary As Model
+
+    Sub New(uri As ConnectionUri)
+        graph = New Model("graph", uri)
+        hash_index = New Model("hash_index", uri)
+        knowledge = New Model("knowledge", uri)
+        knowledge_vocabulary = New Model("knowledge_vocabulary", uri)
+    End Sub
+
+    Public Function Add(term As String, metadata As Dictionary(Of String, String()))
+
+    End Function
+
+End Class
--- src/graphR/graphMySQL.vb
+++ src/graphR/graphMySQL.vb
@@ -1,37 +1,42 @@
 ﻿
+Imports graph.MySQL
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports Oracle.LinuxCompatibility.MySQL.Uri
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("graph_mysql")>
-Public Module graphMySQL
+Public Module graphMySQLTool
 
     <ExportAPI("open.graphdb")>
-    <RApiReturn("graph", "hash_index", "knowledge", "knowledge_vocabulary")>
+    <RApiReturn(GetType(graphMySQL))>
     Public Function openGraphdb(user_name As String, password As String,
                                 Optional host As String = "localhost",
                                 Optional port As Integer = 3306,
                                 Optional dbname As String = "graphql") As Object
 
-        Dim uri As New ConnectionUri() With {
+        Return New graphMySQL(uri:=New ConnectionUri() With {
             .Database = dbname,
             .IPAddress = host,
             .Password = password,
             .Port = port,
             .User = user_name
-        }
+        })
+    End Function
 
-        Return New list With {
-            .slots = New Dictionary(Of String, Object) From {
-                {"graph", New Model("graph", uri)},
-                {"hash_index", New Model("hash_index", uri)},
-                {"knowledge", New Model("knowledge", uri)},
-                {"knowledge_vocabulary", New Model("knowledge_vocabulary", uri)}
-            }
-        }
+    <ExportAPI("add_term")>
+    Public Function add_term(graphdb As graphMySQL,
+                             term As String,
+                             <RListObjectArgument>
+                             Optional metadata As list = Nothing,
+                             Optional env As Environment = Nothing) As Object
+
+        Dim meta_str As Dictionary(Of String, String()) = metadata.AsGeneric(Of String())(env)
+        Dim result = graphdb.Add(term, meta_str)
+
+        Return result
     End Function
 
 End Module
