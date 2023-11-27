e104293b634878a6420ed59f5c5234962869382d
谢桂纲 <gg.xie@bionovogene.com>
2023-11-27 08:53:48 +0800

    split source file text

--- src/graphMySQL/graphMySQL.vb
+++ src/graphMySQL/graphMySQL.vb
@@ -7,29 +7,6 @@ Imports Microsoft.VisualBasic.Linq
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports Oracle.LinuxCompatibility.MySQL.Uri
 
-Public MustInherit Class graphdbMySQL
-
-    Public ReadOnly Property graph As Model
-    Public ReadOnly Property hash_index As Model
-    Public ReadOnly Property knowledge As Model
-    Public ReadOnly Property knowledge_vocabulary As Model
-
-    Sub New(uri As ConnectionUri)
-        graph = New Model("graph", uri)
-        hash_index = New Model("hash_index", uri)
-        knowledge = New Model("knowledge", uri)
-        knowledge_vocabulary = New Model("knowledge_vocabulary", uri)
-    End Sub
-
-    Protected Sub New(graph As Model, hash_index As Model, knowledge As Model, knowledge_vocabulary As Model)
-        Me.graph = graph
-        Me.hash_index = hash_index
-        Me.knowledge = knowledge
-        Me.knowledge_vocabulary = knowledge_vocabulary
-    End Sub
-
-End Class
-
 Public Class graphMySQL : Inherits graphdbMySQL
 
     ReadOnly vocabulary_cache As New Dictionary(Of String, UInteger)
