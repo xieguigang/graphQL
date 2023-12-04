0b6733cdc19d5ec33d8739c4ddbd4d6bc6449094
谢桂纲 <gg.xie@bionovogene.com>
2023-12-04 09:27:27 +0800

    show updates

--- src/web/mysql/update.cmd
+++ src/web/mysql/update.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-SET reflector="D:\graphQL\src\mysqli\App\Reflector.exe"
+SET reflector="\graphQL\src\mysqli\App\Reflector.exe"
 SET current_schema="./text_mining_current.sql"
 SET updates_schema="./text_mining.sql"
 SET updates_report="./text_mining_current_upgrades_to_text_mining.schema_compares.report.md"
