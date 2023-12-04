deb93008514cd7ff48696d880f60928f6bec8118
guigang xie <xie.guigang@gcmodeller.org>
2023-12-04 11:06:31 +0800

    make improvements of the type handler

--- src/graphMySQL/graphdbMySQL.vb
+++ src/graphMySQL/graphdbMySQL.vb
@@ -1,7 +1,8 @@
-﻿Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports Oracle.LinuxCompatibility.MySQL.Uri
 
-Public MustInherit Class graphdbMySQL
+Public MustInherit Class graphdbMySQL : Inherits IDatabase
 
     Public ReadOnly Property graph As Model
     Public ReadOnly Property hash_index As Model
@@ -9,6 +10,8 @@ Public MustInherit Class graphdbMySQL
     Public ReadOnly Property knowledge_vocabulary As Model
 
     Sub New(uri As ConnectionUri)
+        Call MyBase.New(uri)
+
         graph = New Model("graph", uri)
         hash_index = New Model("hash_index", uri)
         knowledge = New Model("knowledge", uri)
@@ -16,6 +19,8 @@ Public MustInherit Class graphdbMySQL
     End Sub
 
     Protected Sub New(graph As Model, hash_index As Model, knowledge As Model, knowledge_vocabulary As Model)
+        Call MyBase.New(Nothing)
+
         Me.graph = graph
         Me.hash_index = hash_index
         Me.knowledge = knowledge
---
+++ src/graphMySQL/mysql.vb
@@ -0,0 +1,13 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Public Class mysql : Inherits graphdbMySQL
+
+    Public ReadOnly Property knowledge_cache As Model
+
+    Public Sub New(uri As ConnectionUri)
+        MyBase.New(uri)
+        knowledge_cache = New Model("knowledge_cache", uri)
+    End Sub
+
+End Class
