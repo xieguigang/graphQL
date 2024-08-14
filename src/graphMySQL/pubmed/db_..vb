41bc420bfe7cc1305f9202cc25a60272dfb2e6ca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-14 21:27:52 +0800

    not working as expected

--- src/graphMySQL/pubmed/db_..vb
+++ src/graphMySQL/pubmed/db_..vb
@@ -1,22 +1,22 @@
-﻿'Imports Oracle.LinuxCompatibility.MySQL
-'Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
-'Imports Oracle.LinuxCompatibility.MySQL.Uri
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Uri
 
-'Namespace pubmed
+Namespace pubmed
 
-'Public MustInherit Class db_. : Inherits IDatabase
-'Protected ReadOnly articles As Model
-'Protected ReadOnly fulltext As Model
-'Protected ReadOnly mesh As Model
-'Protected ReadOnly metadata As Model
-'Protected Sub New(mysqli As ConnectionUri)
-'Call MyBase.New(mysqli)
+Public MustInherit Class db_. : Inherits IDatabase
+Protected ReadOnly m_articles As Model
+Protected ReadOnly m_fulltext As Model
+Protected ReadOnly m_mesh As Model
+Protected ReadOnly m_metadata As Model
+Protected Sub New(mysqli As ConnectionUri)
+Call MyBase.New(mysqli)
 
-'Me.articles = model(Of articles)()
-'Me.fulltext = model(Of fulltext)()
-'Me.mesh = model(Of mesh)()
-'Me.metadata = model(Of metadata)()
-'End Sub
-'End Class
+Me.m_articles = model(Of articles)()
+Me.m_fulltext = model(Of fulltext)()
+Me.m_mesh = model(Of mesh)()
+Me.m_metadata = model(Of metadata)()
+End Sub
+End Class
 
-'End Namespace
+End Namespace
