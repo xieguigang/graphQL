5f84f20bf384f3a12273faa7d7e1072fa4d94d33
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-14 17:31:24 +0800

    test precursor matches bug

--- src/graphMySQL/graphdb/db_..vb
+++ src/graphMySQL/graphdb/db_..vb
@@ -1,24 +1,24 @@
-﻿Imports Oracle.LinuxCompatibility.MySQL
-Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
-Imports Oracle.LinuxCompatibility.MySQL.Uri
+﻿'Imports Oracle.LinuxCompatibility.MySQL
+'Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+'Imports Oracle.LinuxCompatibility.MySQL.Uri
 
-Namespace graphdb
+'Namespace graphdb
 
-Public MustInherit Class db_. : Inherits IDatabase
-Protected ReadOnly graph As Model
-Protected ReadOnly hash_index As Model
-Protected ReadOnly knowledge As Model
-Protected ReadOnly knowledge_cache As Model
-Protected ReadOnly knowledge_vocabulary As Model
-Protected Sub New(mysqli As ConnectionUri)
-Call MyBase.New(mysqli)
+'Public MustInherit Class db_. : Inherits IDatabase
+'Protected ReadOnly graph As Model
+'Protected ReadOnly hash_index As Model
+'Protected ReadOnly knowledge As Model
+'Protected ReadOnly knowledge_cache As Model
+'Protected ReadOnly knowledge_vocabulary As Model
+'Protected Sub New(mysqli As ConnectionUri)
+'Call MyBase.New(mysqli)
 
-Me.graph = model(Of graph)()
-Me.hash_index = model(Of hash_index)()
-Me.knowledge = model(Of knowledge)()
-Me.knowledge_cache = model(Of knowledge_cache)()
-Me.knowledge_vocabulary = model(Of knowledge_vocabulary)()
-End Sub
-End Class
+'Me.graph = model(Of graph)()
+'Me.hash_index = model(Of hash_index)()
+'Me.knowledge = model(Of knowledge)()
+'Me.knowledge_cache = model(Of knowledge_cache)()
+'Me.knowledge_vocabulary = model(Of knowledge_vocabulary)()
+'End Sub
+'End Class
 
-End Namespace
+'End Namespace
