c22e21ae1e9a25a686372b490f09a480892b1d38
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-14 11:48:54 +0800

    make updates of the database model

--- src/graphMySQL/graphdb/mysql_model.cmd
+++ src/graphMySQL/graphdb/mysql_model.cmd
@@ -1,7 +1,7 @@
 REM @echo off
 
 SET Reflector="../../mysqli/App/Reflector.exe"
-SET sql_src="../../web/mysql/graphdb.sql"
+SET sql_src="../../../mysql/graphdb.sql"
 
 %Reflector% --reflects /sql %sql_src% -o ./ /namespace graphdb --language visualbasic /split
 
