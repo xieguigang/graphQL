e09355b72b549eaca6d0f90e8d3f57a20c074763
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-14 12:25:52 +0800

    export pubmed database model sql onbjects

---
+++ src/graphMySQL/pubmed/db_..vb
@@ -0,0 +1,22 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Namespace pubmed
+
+Public MustInherit Class db_. : Inherits IDatabase
+Protected ReadOnly articles As Model
+Protected ReadOnly fulltext As Model
+Protected ReadOnly mesh As Model
+Protected ReadOnly metadata As Model
+Protected Sub New(mysqli As ConnectionUri)
+Call MyBase.New(mysqli)
+
+Me.articles = model(Of articles)()
+Me.fulltext = model(Of fulltext)()
+Me.mesh = model(Of mesh)()
+Me.metadata = model(Of metadata)()
+End Sub
+End Class
+
+End Namespace
--- src/graphMySQL/pubmed/mysql_model.cmd
+++ src/graphMySQL/pubmed/mysql_model.cmd
@@ -1,8 +1,8 @@
 REM @echo off
 
 SET Reflector="../../mysqli/App/Reflector.exe"
-SET sql_src="../../../mysql/graphdb.sql"
+SET sql_src="../../../mysql/pubmed.sql"
 
-%Reflector% --reflects /sql %sql_src% -o ./ /namespace graphdb --language visualbasic /split
+%Reflector% --reflects /sql %sql_src% -o ./ /namespace pubmed --language visualbasic /split
 
 pause
