9c487d5c9df8545eda3ca7b777397be484d0012e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-15 11:22:26 +0800

    save database

--- R/pubmed/load_pubmed.R
+++ R/pubmed/load_pubmed.R
@@ -25,8 +25,59 @@ const open_pubmed = function(user, passwd,
 #' 
 const load_pubmed = function(pubmed, file) {
     let articles = parse.article_set(file);
+    let article_tbl = pubmed |> table("articles");
+    let abstract_tbl = pubmed |> table("fulltext");
+    let mesh_tbl = pubmed |> table("mesh");
+    let graph = pubmed |> table("metadata");
 
     for(let article in tqdm(articles)) {
-        
+        article <- summary(article);
+
+        if (!article_tbl |> check(id = article$PMID)) {
+            article_tbl |> add(
+                id = article$PMID,
+                authors = paste(article$authors, sep = ", "),
+                title = article$title,
+                journal = article$journal,
+                doi = article$doi,
+                year = article$year 
+            );  
+        }
+
+        if (!abstract_tbl |> check(id = article$PMID)) {
+            abstract_tbl |> add(
+                id = article$PMID,
+                abstract = article$abstract
+            );
+        }
+
+        let mesh_terms = article$mesh;
+
+        for(let term_id in names(mesh_terms)) {
+            let term = mesh_terms[[term_id]];
+
+            if (!mesh_tbl |> check(mesh_id = term_id)) {
+                mesh_tbl |> add(
+                    mesh_id = term_id,
+                    term = term
+                );
+            }
+
+            term_id = mesh_tbl 
+            |> where(mesh_id = term_id) 
+            |> find()
+            ;
+
+            if (is.null(term_id)) {
+                print(`save mesh term error: '${term}'.`);
+            } else {
+                if (!graph |> check(pubmed_id = article$PMID, mesh_id = term_id$id)) {
+                    graph |> add(
+                        pubmed_id = article$PMID, 
+                        mesh_id = term_id$id
+                    );
+                }
+            }
+        }
     }
 }
--- test/pubmed_test.R
+++ test/pubmed_test.R
@@ -4,4 +4,9 @@ imports "pubmed" from "kb";
 
 let articles = parse.article_set("F:\\pubmed\\pubmed24n0002.xml.gz");
 
-str(as.data.frame(articles));
+# str(as.data.frame(articles));
+
+for(let article in articles[1:100]) {
+str(summary(article));
+}
+
